f93156cd3e540db8a32ee66932eeb9999ff25b37
Ethan <ysa4@aluno.ifal.edu.br>
2022-07-30 19:48:49 -0300

    testando evento drag

--- src/components/Horarios.vue
+++ src/components/Horarios.vue
@@ -2,6 +2,26 @@
     <section id="horarios" class="horarios">
         <div class="divisor-triangular"></div>
         <h2>Horários de aula</h2>
-        <img src="@/assets/imgs/HORARIO-OFICIAL-923A.jpg" alt="Horário das aulas da turma 923A">
+        <img
+            src="@/assets/imgs/HORARIO-OFICIAL-923A.jpg"
+            alt="Horário das aulas da turma 923A"
+            @dragstart="calculateInitialDistance"
+        />
+        <p v-if="teste">funcionou</p>
     </section>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                teste: false,
+            }
+        },
+        methods: {
+            calculateInitialDistance(ev) {
+                this.teste = true;
+            }
+        }
+    }
+</script>
