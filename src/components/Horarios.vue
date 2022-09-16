9c4e027516d06ffd618b7d4a219bf4911b580ee8
Ethan <ysa4@aluno.ifal.edu.br>
2022-09-15 23:55:00 -0300

    reposicionando horario

--- src/components/Horarios.vue
+++ src/components/Horarios.vue
@@ -2,9 +2,12 @@
     <section id="horarios" class="horarios">
         <div class="divisor-triangular"></div>
         <h2>Horários de aula</h2>
-        <img
-            src="@/assets/imgs/HORARIO-OFICIAL-923A.jpg"
-            alt="Horário das aulas da turma 923A"
-        />
+
+        <div class="horarios__conteiner">
+            <img
+                src="@/assets/imgs/HORARIO-OFICIAL-923A.jpg"
+                alt="Horário das aulas da turma 923A"
+            />
+        </div>
     </section>
 </template>
